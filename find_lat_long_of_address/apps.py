aa051dc2b16979c982e879dcdcf984cfa18a8253
Panayotis Matsinopoulos <>
2018-07-20 21:51:23 +0300

    Initial commit

---
+++ find_lat_long_of_address/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class FindLatLongOfAddressConfig(AppConfig):
+    name = 'find_lat_long_of_address'
